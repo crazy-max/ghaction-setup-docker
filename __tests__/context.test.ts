dcb7246e5fd282c2a08dd00027943d132ed5b673
CrazyMax <crazy-max@users.noreply.github.com>
2023-03-02 14:15:43 +0100

    basic test for context

---
+++ __tests__/context.test.ts
@@ -0,0 +1,45 @@
+import {beforeEach, describe, expect, test} from '@jest/globals';
+
+import * as context from '../src/context';
+
+describe('getInputs', () => {
+  beforeEach(() => {
+    process.env = Object.keys(process.env).reduce((object, key) => {
+      if (!key.startsWith('INPUT_')) {
+        object[key] = process.env[key];
+      }
+      return object;
+    }, {});
+  });
+
+  // prettier-ignore
+  test.each([
+    [
+      0,
+      new Map<string, string>([
+        ['version', '23.0.1'],
+      ]),
+      {
+        version: '23.0.1',
+      } as context.Inputs
+    ]
+  ])(
+    '[%d] given %p as inputs, returns %p',
+    async (num: number, inputs: Map<string, string>, expected: context.Inputs) => {
+      inputs.forEach((value: string, name: string) => {
+        setInput(name, value);
+      });
+      const res = await context.getInputs();
+      expect(res).toEqual(expected);
+    }
+  );
+});
+
+// See: https://github.com/actions/toolkit/blob/master/packages/core/src/core.ts#L67
+function getInputName(name: string): string {
+  return `INPUT_${name.replace(/ /g, '_').toUpperCase()}`;
+}
+
+function setInput(name: string, value: string): void {
+  process.env[getInputName(name)] = value;
+}
